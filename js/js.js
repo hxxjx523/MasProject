05f58800762f93634bd484dd05330096bc211276
heeju <hhj05230@gmail.com>
2023-08-23 15:54:09 +0900

    profile, skills, project

--- js/js.js
+++ js/js.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function(){
     var scroll = window.scrollY;
-    if(scroll > 200) {
+    if(scroll > 250) {
         document.querySelector('.name').style.fontSize = '30px';
-        document.querySelector('.name').style.width = '1200px';
+        document.querySelector('.name').style.width = '95%';
         document.querySelector('.name').style.height = '40px';
         document.querySelector('.name').style.transition = '0.5s';
         document.querySelector('.name').style.borderBottom = '1.5px black solid';
@@ -15,25 +15,5 @@ window.addEventListener('scroll', function(){
     }
 });
 
-$(window).ready(function(){
-    draw(80, '.pie-chart1', '#ccc');
-   draw(50, '.pie-chart2', '#8b22ff');
-   draw(30, '.pie-chart3','#ff0');
- });
- 
- function draw(max, classname, colorname){
-    var i=1;
-     var func1 = setInterval(function(){
-       if(i<max){
-           color1(i,classname,colorname);
-           i++;
-       } else{
-         clearInterval(func1);
-       }
-     },10);
- }
- function color1(i, classname,colorname){
-    $(classname).css({
-         "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
-    });
- }
+
+
